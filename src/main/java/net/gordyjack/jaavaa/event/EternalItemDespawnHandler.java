1a981e4b631acd0ffa9b7ba13d88471b190ebede
gordyjack <gordyjack.mail@gmail.com>
2025-01-02 00:57:58 -0600

    Modified to be more inline with other classes.

--- src/main/java/net/gordyjack/jaavaa/event/EternalItemDespawnHandler.java
+++ src/main/java/net/gordyjack/jaavaa/event/EternalItemDespawnHandler.java
@@ -1,12 +1,14 @@
 package net.gordyjack.jaavaa.event;
 
 import net.fabricmc.fabric.api.event.lifecycle.v1.*;
+import net.gordyjack.jaavaa.*;
 import net.gordyjack.jaavaa.item.custom.*;
 import net.minecraft.entity.*;
 import net.minecraft.item.*;
 
 public class EternalItemDespawnHandler {
-    public static void register() {
+    public static void init() {
+        JAAVAA.log("Initializing Eternal Item Despawn Handler");
         ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
             if (entity instanceof ItemEntity itemEntity) {
                 ItemStack stack = itemEntity.getStack();
