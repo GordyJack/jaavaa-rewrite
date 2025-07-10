286e10a6fd099c436738f9dc50cbece18a6039ff
gordyjack <gordyjack.mail@gmail.com>
2025-07-10 00:25:29 -0500

    Modified CurseOfUnbridledDestructionEffect to account for HammerItems.

--- src/main/java/net/gordyjack/jaavaa/enchantment/CurseOfUnbridledDestructionEffect.java
+++ src/main/java/net/gordyjack/jaavaa/enchantment/CurseOfUnbridledDestructionEffect.java
@@ -2,6 +2,8 @@ package net.gordyjack.jaavaa.enchantment;
 
 import com.mojang.serialization.*;
 import com.mojang.serialization.codecs.*;
+import net.gordyjack.jaavaa.data.*;
+import net.gordyjack.jaavaa.item.custom.*;
 import net.gordyjack.jaavaa.utils.*;
 import net.minecraft.block.*;
 import net.minecraft.enchantment.*;
@@ -23,17 +25,29 @@ public record CurseOfUnbridledDestructionEffect(EnchantmentLevelBasedValue level
     @Override
     public void apply(ServerWorld serverWorld, int level, EnchantmentEffectContext context, Entity user, Vec3d pos) {
         if (user instanceof ServerPlayerEntity player && !player.isCreative()) {
-            BlockPos blockPos = new BlockPos((int) Math.floor(pos.x), (int) Math.floor(pos.y), (int) Math.floor(pos.z));
-            BlockState currentState = serverWorld.getBlockState(blockPos);
+            BlockPos hitPos = new BlockPos((int) Math.floor(pos.x), (int) Math.floor(pos.y), (int) Math.floor(pos.z));
+            BlockState currentState = serverWorld.getBlockState(hitPos);
             ItemStack tool = player.getMainHandStack();
 
-            if (JAAVAAUtils.isToolCorrectForBlock(tool, currentState, false)
-                    && serverWorld.breakBlock(blockPos, false, player)) {
-                currentState.getBlock().onBreak(serverWorld, blockPos, serverWorld.getBlockState(blockPos), player);
-                JAAVAAUtils.damageBreakable(tool, 3, serverWorld, player);
+            if (tool.getItem() instanceof HammerItem hammer) {
+                var blocksToDestroy = HammerItem.getBlocksToBeDestroyed(hammer.getComponents().get(JAAVAAComponents.Types.HAMMER_RANGE), hitPos, player);
+                for (var blockPos : blocksToDestroy) {
+                    mineBlock(serverWorld, player, tool, currentState, blockPos);
+                }
+            } else {
+                mineBlock(serverWorld, player, tool, currentState, hitPos);
             }
         }
     }
+
+    private static void mineBlock(ServerWorld serverWorld, ServerPlayerEntity player, ItemStack tool, BlockState currentState, BlockPos blockPos) {
+        if (JAAVAAUtils.isToolCorrectForBlock(tool, currentState, false)
+                && serverWorld.breakBlock(blockPos, false, player)) {
+            currentState.getBlock().onBreak(serverWorld, blockPos, serverWorld.getBlockState(blockPos), player);
+            JAAVAAUtils.damageBreakable(tool, 3, serverWorld, player);
+        }
+    }
+
     @Override
     public MapCodec<? extends EnchantmentEntityEffect> getCodec() {
         return CODEC;
