b3d68026cd821d78fcd79baee62c751af4be35e1
gordyjack <gordyjack.mail@gmail.com>
2025-07-12 20:49:43 -0500

    Fixed Waterlogging on Blocktants.

--- src/main/java/net/gordyjack/jaavaa/block/custom/Blocktant.java
+++ src/main/java/net/gordyjack/jaavaa/block/custom/Blocktant.java
@@ -20,12 +20,43 @@ import org.jetbrains.annotations.*;
 public class Blocktant
 extends Block
 implements Waterloggable, VoxelShapeUtils {
+    //Constants
     public static final IntProperty POSITION = JAAVAABlockProperties.BLOCKTANT_POSITION;
     public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
     private static final VoxelShape BASE_SHAPE = Block.createCuboidShape(0, 0, 0, 8, 8, 8);
+    public static final int FULL_POS = 0b11111111;
 
+    //Constructor
     public Blocktant(Settings settings) {
         super(settings);
+        this.setDefaultState(this.getStateManager().getDefaultState()
+                .with(POSITION, 0b00000001) // Default to Down North West
+                .with(WATERLOGGED, false));
+    }
+
+    //Overrides
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(POSITION, WATERLOGGED);
+    }
+    @Override
+    protected boolean canPathfindThrough(BlockState state, NavigationType type) {
+        return switch (type) {
+            case LAND, AIR -> false;
+            case WATER -> state.get(WATERLOGGED);
+        };
+    }
+    @Override
+    public boolean canReplace(BlockState state, ItemPlacementContext context) {
+        int existingPosition = state.get(POSITION);
+
+        if (existingPosition == FULL_POS || !context.getStack().isOf(this.asItem())) {
+            return false;
+        } else if (context.canReplaceExisting()) {
+            return (existingPosition & getPlacePosition(context)) == 0;
+        } else {
+            return true;
+        }
     }
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
@@ -82,23 +113,52 @@ implements Waterloggable, VoxelShapeUtils {
         int newPosition = existingPosition | placePosition;
         
         return blockState.with(POSITION, newPosition)
-                .with(WATERLOGGED, fluidState.getFluid() == Fluids.WATER);
+                .with(WATERLOGGED, fluidState.getFluid() == Fluids.WATER && newPosition != FULL_POS);
     }
+    @Override
+    protected BlockState getStateForNeighborUpdate(BlockState state, WorldView world, ScheduledTickView tickView,
+                                                   BlockPos pos, Direction direction, BlockPos neighborPos,
+                                                   BlockState neighborState, Random random) {
+        if (state.get(WATERLOGGED)) {
+            tickView.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+        return super.getStateForNeighborUpdate(state, world, tickView, pos, direction, neighborPos, neighborState, random);
+    }
+    @Override
+    public boolean hasSidedTransparency(BlockState state) {
+        return state.get(POSITION) != FULL_POS;
+    }
+
+    //Waterloggable overrides
+    @Override
+    public boolean canFillWithFluid(@Nullable LivingEntity filler, BlockView world, BlockPos pos, BlockState state, Fluid fluid) {
+        return state.get(POSITION) != FULL_POS && Waterloggable.super.canFillWithFluid(filler, world, pos, state, fluid);
+    }
+    @Override
+    protected FluidState getFluidState(BlockState state) {
+        return state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
+    }
+    @Override
+    public boolean tryFillWithFluid(WorldAccess world, BlockPos pos, BlockState state, FluidState fluidState) {
+        return state.get(POSITION) != FULL_POS && Waterloggable.super.tryFillWithFluid(world, pos, state, fluidState);
+    }
+
+    //Methods
     private static int getPlacePosition(ItemPlacementContext ctx) {
         Vec3d hitPos = ctx.getHitPos();
         double xHitPos = hitPos.x;
         double yHitPos = hitPos.y;
         double zHitPos = hitPos.z;
-        
+
         BlockPos blockPos = ctx.getBlockPos();
         double xBlockPos = blockPos.getX();
         double yBlockPos = blockPos.getY();
         double zBlockPos = blockPos.getZ();
-        
+
         double relativeXPos = xHitPos - xBlockPos;
         double relativeYPos = yHitPos - yBlockPos;
         double relativeZPos = zHitPos - zBlockPos;
-        
+
         Direction side = ctx.getSide();
         if (relativeZPos == 0.5 && side == Direction.SOUTH) {
             relativeZPos = Math.nextUp(relativeZPos);
@@ -109,9 +169,7 @@ implements Waterloggable, VoxelShapeUtils {
         if (relativeXPos == 0.5 && side == Direction.EAST) {
             relativeXPos = Math.nextUp(relativeXPos);
         }
-        return getPlacePosition(relativeZPos, relativeYPos, relativeXPos);
-    }
-    private static int getPlacePosition(double relativeZPos, double relativeYPos, double relativeXPos) {
+
         boolean north = relativeZPos <= .5;
         boolean south = relativeZPos > .5;
         boolean down = relativeYPos <=.5;
@@ -139,55 +197,5 @@ implements Waterloggable, VoxelShapeUtils {
             placePosition = 0b10000000;
         }
         return placePosition;
-    }
-    @Override
-    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(POSITION, WATERLOGGED);
-    }
-    @Override
-    public boolean hasSidedTransparency(BlockState state) {
-        return state.get(POSITION) != 0b11111111;
-    }
-    @Override
-    public boolean canReplace(BlockState state, ItemPlacementContext context) {
-        int existingPosition = state.get(POSITION);
-
-        if (existingPosition == 0b11111111 || !context.getStack().isOf(this.asItem())) {
-            return false;
-        } else if (context.canReplaceExisting()) {
-            return (existingPosition & getPlacePosition(context)) == 0;
-        } else {
-            return true;
-        }
-    }
-    @Override
-    public boolean tryFillWithFluid(WorldAccess world, BlockPos pos, BlockState state, FluidState fluidState) {
-        if (state.get(POSITION) != 0b11111111) {
-            return Waterloggable.super.tryFillWithFluid(world, pos, state, fluidState);
-        }
-        return false;
-    }
-    @Override
-    public boolean canFillWithFluid(@Nullable LivingEntity filler, BlockView world, BlockPos pos, BlockState state, Fluid fluid) {
-        if (state.get(POSITION) != 0b11111111) {
-            return Waterloggable.super.canFillWithFluid(filler, world, pos, state, fluid);
-        }
-        return false;
-    }
-    @Override
-    protected boolean canPathfindThrough(BlockState state, NavigationType type) {
-        return switch (type) {
-            case LAND, AIR -> false;
-            case WATER -> state.get(WATERLOGGED);
-        };
-    }
-    @Override
-    protected BlockState getStateForNeighborUpdate(BlockState state, WorldView world, ScheduledTickView tickView,
-                                                   BlockPos pos, Direction direction, BlockPos neighborPos,
-                                                   BlockState neighborState, Random random) {
-        if (state.get(WATERLOGGED)) {
-            tickView.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
-        }
-        return super.getStateForNeighborUpdate(state, world, tickView, pos, direction, neighborPos, neighborState, random);
     }
 }
