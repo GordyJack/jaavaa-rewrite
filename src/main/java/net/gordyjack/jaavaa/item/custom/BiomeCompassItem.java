0a44a0d31d8903e8815b3ea6b4aec0b0caa20b0b
gordyjack <gordyjack.mail@gmail.com>
2025-07-17 21:51:25 -0500

    Changed the BiomeCompass to search for biomes using similar logic as the vanilla locate command.

--- src/main/java/net/gordyjack/jaavaa/item/custom/BiomeCompassItem.java
+++ src/main/java/net/gordyjack/jaavaa/item/custom/BiomeCompassItem.java
@@ -1,31 +1,27 @@
 package net.gordyjack.jaavaa.item.custom;
 
+import com.mojang.datafixers.util.Pair;
 import net.gordyjack.jaavaa.data.*;
-import net.gordyjack.jaavaa.item.utils.*;
 import net.minecraft.component.type.*;
-import net.minecraft.entity.*;
 import net.minecraft.entity.player.*;
 import net.minecraft.item.*;
 import net.minecraft.item.tooltip.*;
+import net.minecraft.registry.*;
+import net.minecraft.registry.entry.*;
 import net.minecraft.server.world.*;
 import net.minecraft.text.*;
 import net.minecraft.util.*;
 import net.minecraft.util.math.*;
 import net.minecraft.world.*;
 import net.minecraft.world.biome.*;
-import org.jetbrains.annotations.*;
 
 import java.util.function.*;
 
 public class BiomeCompassItem
         extends Item {
-    private SpiralTickSearcher searcher;
-    public boolean searching = false;
 
     public BiomeCompassItem(Settings settings) {
         super(settings);
-        this.searcher = new SpiralTickSearcher(null, null, this.getDefaultStack()
-                .get(JAAVAAComponents.Types.BIOME_COMPASS_TARGET)); // Placeholder, will be set in inventoryTick
     }
 
     @Override
@@ -36,8 +32,6 @@ public class BiomeCompassItem
             textConsumer.accept(Text.literal("§7Attuned to: §b" + biome.getValue().getPath()));
             if (biomePosition != null){
                 textConsumer.accept(Text.literal("§7Biome found at: §e" + biomePosition.toShortString()));
-            } else if (searching) {
-                textConsumer.accept(Text.literal("§7Searching... §e%" + (int)((searcher.currentIndex() / (float) searcher.maxIndex()) * 100)));
             } else {
                 textConsumer.accept(Text.literal("§7No biome matching §b" + biome.getValue().getPath() + "§7 found."));
             }
@@ -48,29 +42,25 @@ public class BiomeCompassItem
         return stack.get(JAAVAAComponents.Types.BIOME_COMPASS_POSITION) != null;
     }
     @Override
-    public void inventoryTick(ItemStack stack, ServerWorld world, Entity entity, @Nullable EquipmentSlot slot) {
-        super.inventoryTick(stack, world, entity, slot);
-        if (stack.getItem() instanceof BiomeCompassItem && searching) {
-            BlockPos newBiomePosition = searcher.tick();
-            if (newBiomePosition == null) {
-                stack.remove(JAAVAAComponents.Types.BIOME_COMPASS_POSITION);
-                searching = false; // Stop searching if no position found
-            } else if (newBiomePosition != BlockPos.ORIGIN) {
-                stack.set(JAAVAAComponents.Types.BIOME_COMPASS_POSITION, newBiomePosition);
-                searching = false;
-            }
-        }
-    }
-    @Override
     public ActionResult use(World world, PlayerEntity user, Hand hand) {
-        if (world.isClient) return ActionResult.PASS;
+        if (!(world instanceof ServerWorld serverWorld)) return ActionResult.PASS;
         ItemStack stack = user.getStackInHand(hand);
         if (stack.getItem() instanceof BiomeCompassItem) {
-            stack.remove(JAAVAAComponents.Types.BIOME_COMPASS_POSITION);
-            this.searcher = new SpiralTickSearcher(world, user, stack.get(JAAVAAComponents.Types.BIOME_COMPASS_TARGET));
-            searching = true;
-            return ActionResult.SUCCESS_SERVER;
+            if (user.isSneaking()) {
+                stack.remove(JAAVAAComponents.Types.BIOME_COMPASS_POSITION);
+                return ActionResult.SUCCESS_SERVER;
+            } else if (stack.get(JAAVAAComponents.Types.BIOME_COMPASS_POSITION) == null) {
+                stack.set(JAAVAAComponents.Types.BIOME_COMPASS_POSITION,
+                        this.locateBiome(serverWorld, user, stack.get(JAAVAAComponents.Types.BIOME_COMPASS_TARGET))
+                );
+                return ActionResult.SUCCESS_SERVER;
+            }
         }
         return super.use(world, user, hand);
+    }
+    private BlockPos locateBiome(ServerWorld serverWorld, PlayerEntity player, RegistryKey<Biome> targetBiome) {
+        Pair<BlockPos, RegistryEntry<Biome>> posBiomePair = serverWorld.locateBiome(biomeRegistryEntry -> biomeRegistryEntry.matchesKey(targetBiome),
+                player.getBlockPos(), 256/*Blocks*/ * 16/*to chunks*/, 32, 64);
+        return posBiomePair.getFirst() != null ? posBiomePair.getFirst() : null;
     }
 }
